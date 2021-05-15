49b23e324f37817f2224986058615e9edc4186c0
rami.atrouni <ramiatrouni@gmail.com>
2021-05-15 15:55:46 +0300

    Save the document in realtime

--- server/server.js
+++ server/server.js
@@ -16,18 +16,28 @@ const io = require('socket.io')(3001, {
 });
 
 io.on("connection", socket => {
-    socket.on("get-document", documentId => {
-        const data = '';
+    socket.on("get-document", async documentId => {
+        const document = await findOrCreateDocument(documentId);
 
         socket.join(documentId)
-        socket.emit('load-document', data);
+        socket.emit('load-document', document.data);
 
         socket.on("send-changes", delta => {
             socket.broadcast.to(documentId).emit("receive-changes", delta);
         });
+
+        socket.on("save-document", async data => {
+            await Document.findByIdAndUpdate(documentId, { data });
+        })
     })
 });
 
-const findOrCreateDocument = () => {
+const findOrCreateDocument = async (id) => {
+    if (!id) return;
+    const defaultValue = '';
 
+    const document = await Document.findById(id);
+    if (document) return document;
+
+    return await Document.create({ _id: id, data: defaultValue })
 }
