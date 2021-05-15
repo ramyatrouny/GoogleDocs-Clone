aaf471550f52dba906131ab681b826cf7d2028d4
rami.atrouni <ramiatrouni@gmail.com>
2021-05-15 15:23:57 +0300

    configure 2 main routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
 import TextEditor from "./TextEditor";
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { v4 as uuidV4 } from 'uuid';
 
 function App() {
   return (
-    <div className="App">
-      <TextEditor />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to={`/documents/${uuidV4()}`} />
+        </Route>
+
+        <Route path="/documents/:id">
+          <TextEditor />
+        </Route>
+      </Switch>
+
+
+    </Router>
   );
 }
 
