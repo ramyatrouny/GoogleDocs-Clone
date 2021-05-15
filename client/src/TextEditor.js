c84578b7b0cf459e2cbb783fe80db3c139c65573
rami.atrouni <ramiatrouni@gmail.com>
2021-05-15 14:32:36 +0300

    Configure quill with more options and functionality

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -2,6 +2,18 @@ import React, { useCallback } from 'react'
 import Quill from 'quill'
 import 'quill/dist/quill.snow.css'
 
+const TOOLBAR_OPTIONS = [
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    [{ font: [] }],
+    [{ list: "ordered" }, { list: "bullet" }],
+    ["bold", "italic", "underline"],
+    [{ color: [] }, { background: [] }],
+    [{ script: "sub" }, { script: "super" }],
+    [{ align: [] }],
+    ["image", "blockquote", "code-block"],
+    ["clean"]
+]
+
 export default function TextEditor() {
     const wrapperRef = useCallback((wrapper) => {
         if (wrapper === null) return;
@@ -10,7 +22,10 @@ export default function TextEditor() {
         const editor = document.createElement('div');
         wrapper.append(editor);
         new Quill(editor, {
-            theme: 'snow'
+            theme: 'snow',
+            modules: {
+                toolbar: TOOLBAR_OPTIONS
+            }
         });
 
         // sdasaadsdasdsaasd
